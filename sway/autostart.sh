8e5570586ba7ca1e9270436a9d5bc2d8026a0856
saeedhosam <makymaky757@gmail.com>
2025-02-23 22:22:13 +0200

    hello stow

--- sway/autostart.sh
+++
@@ -1,9 +0,0 @@
-#1/bin/bash
-
-copyq
-sleep 1 && swaymsg 'workspace "1: video"; exec google-chrome --profile-directory="Default"'
-sleep 1 && swaymsg 'workspace "2: research"; exec google-chrome --profile-directory="Default"'
-sleep 1 && swaymsg 'workspace "3: whiteboard"; exec google-chrome --profile-directory="Default" --new-window "excalidraw.com"'
-sleep 1 && swaymsg 'workspace "4: ?"; exec google-chrome --profile-directory="Default" --new-window "open.spotify.com"'
-sleep 1 && swaymsg 'workspace "7: notion"; exec google-chrome --profile-directory="Default" --new-window "notion.so"'
-sleep 2 && swaymsg 'workspace "10: code"; exec $term'
