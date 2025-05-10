bb6ef76d4067c3c16de90789e8304c271756a804
Saeed Hosam <makymaky757@gmail.com>
2025-05-10 17:50:42 +0300

    'nvim-tree-mapping'

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,4 +1,5 @@
 require "nvchad.mappings"
+local api = require("nvim-tree.api")
 
 -- add yours here
 
@@ -6,5 +7,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "\\", "<cmd>NvimTreeToggle<CR>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
