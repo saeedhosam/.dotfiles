3fc7a4d2f286bb6e950cfe215f20d798432a4aed
saeed <makymaky757@gmail.com>
2025-05-13 07:12:24 +0300

    kinda complete

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -5,3 +5,9 @@ require "nvchad.options"
 local o = vim.o
 o.cursorlineopt ='both' -- to enable cursorline!
 o.shell = 'nu'
+o.ignorecase = true
+o.smartcase = true
+o.updatetime = 250
+o.timeoutlen = 300
+o.scrolloff = 15
+
