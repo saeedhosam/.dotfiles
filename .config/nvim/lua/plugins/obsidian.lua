407a94338e645992988e7f8a045c5e0b8f2e10a0
Saeed Hosam <makymaky757@gmail.com>
2025-05-14 23:56:54 +0300

    'woeijfew'

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -21,6 +21,10 @@ return {
                 name = 'college',
                 path = '~/OneDrive/Documents/GitHub/obsidian/college',
             },
+            {
+                name = 'cs',
+                path = '~/OneDrive/Documents/GitHub/obsidian/cs',
+            },
         },
     },
 }
