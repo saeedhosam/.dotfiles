41e78075ea326cccd6587f1e13b77744a814d88c
saeed <makymaky757@gmail.com>
2025-05-14 05:46:59 +0300

    iowjefg

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,13 @@
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.g.base46_cache = vim.fn.stdpath 'data' .. '/base46/'
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-require("options")
-require("config.lazy")
+require 'options'
+require 'config.lazy'
 
-dofile(vim.g.base46_cache .. "defaults")
-dofile(vim.g.base46_cache .. "statusline")
-
+dofile(vim.g.base46_cache .. 'defaults')
+dofile(vim.g.base46_cache .. 'statusline')
 
 vim.schedule(function()
-  require "mappings"
+    require 'mappings'
 end)
