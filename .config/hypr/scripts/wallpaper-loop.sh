676acbf4296c9afe4d1596220a8d7cb68ad4ab45
saeedhosam <makymaky757@gmail.com>
2025-05-18 01:40:00 +0300

    owiefjw

--- .config/hypr/scripts/wallpaper-loop.sh
+++ .config/hypr/scripts/wallpaper-loop.sh
@@ -4,6 +4,6 @@ WALL_DIR=~/wallpapers
 while true; do
   pic=$(find "$WALL_DIR" -type f | shuf -n1)
   swww img "$pic" --transition-type any --transition-fps 60 --transition-duration 2
-  sleep 300  # 10 mins
+  sleep 600  # 10 mins
 done
 
