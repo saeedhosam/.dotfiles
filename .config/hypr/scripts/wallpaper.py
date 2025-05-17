ff628325ab83adab4c1f7006b684400f5e02ab3d
saeedhosam <makymaky757@gmail.com>
2025-05-17 23:13:09 +0300

    fixing swww and tmux

---
+++ .config/hypr/scripts/wallpaper.py
@@ -0,0 +1,26 @@
+import os
+import random
+import subprocess
+import time
+
+WALL_DIR = os.path.expanduser("~/wallpapers")
+
+
+def is_daemon_running():
+    return subprocess.run(["pgrep", "swww-daemon"], stdout=subprocess.DEVNULL).returncode == 0
+
+if not is_daemon_running():
+    subprocess.Popen(["swww-daemon"])
+    time.sleep(1)
+
+while True:
+    pics = [f for f in os.listdir(WALL_DIR) if os.path.isfile(os.path.join(WALL_DIR, f))]
+    if pics:
+        pic = os.path.join(WALL_DIR, random.choice(pics))
+        subprocess.run([
+            "swww", "img", pic,
+            "--transition-type", "any",
+            "--transition-fps", "60",
+            "--transition-duration", "2"
+        ])
+    time.sleep(600)
