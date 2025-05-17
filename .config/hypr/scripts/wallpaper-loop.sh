eed6399cab90e198b3739c32e2b95f9632f9ad7b
saeedhosam <makymaky757@gmail.com>
2025-05-17 23:13:09 +0300

    fixing swww and tmux

--- .config/hypr/scripts/wallpaper-loop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-WALL_DIR=~/wallpapers
-
-while true; do
-  pic=$(find "$WALL_DIR" -type f | shuf -n1)
-  swww img "$pic" --transition-type any --transition-fps 60 --transition-duration 2
-  sleep 600  # 10 mins
-done
-
