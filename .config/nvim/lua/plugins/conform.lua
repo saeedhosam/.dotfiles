3bde9cb18aace3488e78c2193bfd47fb7dc2a764
saeedhosam <makymaky757@gmail.com>
2025-05-19 06:14:58 +0300

    formatting, small changes, and scss

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,6 +1,13 @@
 return {
     'stevearc/conform.nvim',
     opts = {
-        formatters_by_ft = { lua = { 'stylua' }, bash = { 'beautysh' } },
+        formatters_by_ft = {
+            lua = { 'stylua' },
+            python = { 'ruff' },
+            scss = { 'prettier' },
+            bash = { 'shfmt' },
+            sh = { 'shfmt' },
+            yuck = { 'yuckfmt' },
+        },
     },
 }
