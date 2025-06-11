9e5b9ae25bf63c9a59da5ee12c019beae1d82d9c
saeedhosam <makymaky757@gmail.com>
2025-06-12 02:05:01 +0300

    hellog

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,3 +14,11 @@ dofile(vim.g.base46_cache .. 'statusline')
 vim.schedule(function()
     require 'mappings'
 end)
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.hl.on_yank()
+    end,
+})
