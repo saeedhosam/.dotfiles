28a88b53c59a005690a7308190c98a2f5b9c1d2e
saeedhosam <makymaky757@gmail.com>
2025-06-18 21:40:52 +0300

    oijwefoiwj

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -4,5 +4,14 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     -- or if using mini.icons/mini.nvim
     -- dependencies = { "echasnovski/mini.icons" },
-    opts = {},
+    opts = {
+        keymap = {
+            -- Below are the default binds, setting any value in these tables will override
+            -- the defaults, to inherit from the defaults change [1] from `false` to `true`
+            builtin = {
+                ['<C-d>'] = 'preview-page-down',
+                ['<C-u>'] = 'preview-page-up',
+            },
+        },
+    },
 }
