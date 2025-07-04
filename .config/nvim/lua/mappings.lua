fb15831774b1a8c175e3ba543935a395e63e4f1a
saeedhosam <makymaky757@gmail.com>
2025-07-04 03:55:56 +0300

    just a moment

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -3,35 +3,36 @@ local map = vim.keymap.set
 -- Essentials
 map('v', '<leader>p', [["_dP]], { noremap = true, silent = true })
 map('n', '<C-[>', '<cmd>noh<CR>')
-map('n', '<C-c>', '<cmd>noh<CR>')
+
 map('n', ';', ':')
 map('n', ':', '')
+
 map('n', '{', '}') -- idk why these were different than jk, at least I don't know 'yet'
 map('n', '}', '{')
 
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, noremap = true, silent = true })
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, noremap = true, silent = true })
+map('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, noremap = true, silent = true })
+map('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, noremap = true, silent = true })
+
+map('n', '<leader>x', '<cmd>so<CR>', { desc = 'Run file' })
 
 -- Windows & buffers
 map('n', '<M-a>', '<C-W>H')
 map('n', '<M-s>', '<C-W>J')
 map('n', '<M-w>', '<C-W>K')
 map('n', '<M-d>', '<C-W>L')
-map('n', '<M-z>', '<C-w>q')
+
 map('n', '<C-h>', '<C-W>h')
 map('n', '<C-j>', '<C-W>j')
 map('n', '<C-k>', '<C-W>k')
 map('n', '<C-l>', '<C-W>l')
-map('n', '<leader>x', '<cmd>so<CR>', { desc = 'Run file' })
-map('t', '<C-[>', '<C-\\><C-N>', { desc = 'terminal escape terminal mode' })
+
 map('n', '<leader>w', '<cmd>close<CR>', { desc = 'Close window' })
 map('n', '<leader>o', '<cmd>only<CR>', { desc = 'Close all windows except current' })
-map('n', '<leader>l', '<cmd>Lazy<CR>', { desc = 'Open Lazy' })
+
 map({ 'n', 'i' }, '<A-.>', '<cmd>BufferLineCycleNext<CR>', { desc = 'buffer go to next' })
 map({ 'n', 'i' }, '<A-,>', '<cmd>BufferLineCyclePrev<CR>', { desc = 'buffer go to previous' })
 map({ 'n', 'i' }, '<A-o>', '<cmd>BufferLineMoveNext<CR>', { desc = 'buffer move to next' })
 map({ 'n', 'i' }, '<A-i>', '<cmd>BufferLineMovePrev<CR>', { desc = 'buffer move to previous' })
-map('n', '<leader>q', '<cmd>bdelete<CR>', { desc = 'buffer close' })
 
 -- Misc
 map('n', '<leader>f', function()
@@ -40,6 +41,9 @@ end, { desc = 'Format file' })
 map('n', '<leader>st', function()
   require('nvchad.themes').open()
 end)
+map('n', '<leader>l', '<cmd>Lazy<CR>', { desc = 'Open Lazy' })
+map('n', '<leader>q', '<cmd>bdelete<CR>', { desc = 'buffer close' })
+map('t', '<C-[>', '<C-\\><C-N>', { desc = 'terminal escape terminal mode' })
 
 -- FzfLua
 local fzf = require 'fzf-lua'
