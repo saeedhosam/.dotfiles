ff9636a4ee952f1d9b77352f803c6c5623778ca1
saeedhosam <makymaky757@gmail.com>
2025-07-14 00:56:03 +0300

    jiowjaefowejif

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,8 +8,9 @@ require 'config.lazy'
 require 'mappings'
 require 'autocmds'
 
--- theme & transparency
-vim.cmd.colorscheme 'tokyonight'
+-- Theme & transparency
+vim.cmd.colorscheme 'tokyonight-night'
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
