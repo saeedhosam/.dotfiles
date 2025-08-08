49ea20862d8f2491d2229f2688e749e7ea690767
saeedhosam <makymaky757@gmail.com>
2025-08-08 12:32:31 +0300

    wusuupjp

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -2,11 +2,23 @@ return {
     { 'Mofiqul/dracula.nvim' },
     {
         'refractalize/oil-git-status.nvim',
-
         dependencies = {
             'stevearc/oil.nvim',
         },
-
         config = true,
+    },
+    -- Plugins for Base46
+    'nvim-lua/plenary.nvim',
+    'nvchad/volt',
+    { 'nvim-tree/nvim-web-devicons', lazy = true },
+    {
+        'nvchad/ui',
+        config = function()
+            require 'nvchad'
+        end,
+    },
+    {
+        'nvchad/base46',
+        lazy = true,
     },
 }
