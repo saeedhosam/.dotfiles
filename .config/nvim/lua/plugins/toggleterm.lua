32672308fe02124481e591271a25c259885529a8
saeedhosam <makymaky757@gmail.com>
2025-08-16 14:04:01 +0300

    woeifjweo

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -2,6 +2,6 @@ return {
     'akinsho/toggleterm.nvim',
     version = '*',
     opts = {--[[ things you want to change go here]]
-        open_mapping = [[<M-'>]], -- or { [[<c-\>]], [[<c-¥>]] } if you also use a Japanese keyboard.
+        open_mapping = [[<M-/>]], -- or { [[<c-\>]], [[<c-¥>]] } if you also use a Japanese keyboard.
     },
 }
